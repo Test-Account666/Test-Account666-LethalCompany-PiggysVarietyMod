0f16346afb55c39316dccc26201e0bee5d4cb6f3
Test-Account666 <36412486+Test-Account666@users.noreply.github.com>
2026-01-02 09:14:31 +0100

    Add random offset for TeslaGate animation

--- PiggysVarietyMod/Hazards/TeslaGate/TeslaGate.cs
+++ PiggysVarietyMod/Hazards/TeslaGate/TeslaGate.cs
@@ -9,10 +9,29 @@ public class TeslaGate : NetworkBehaviour {
     public Animator animator;
     public AudioSource shutDownSound;
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+    private float _randomOffset = -1F;
+
+    #region Overrides of NetworkBehaviour
+
+    public override void OnNetworkSpawn() {
+        if (!IsHost && !IsServer) return;
+        _randomOffset = Random.Range(0F, 1F);
+        UseRandomOffsetClientRpc(_randomOffset);
+    }
+
+    #endregion
+
     private void LateUpdate() {
         if (!animator.GetBool(_POWERED_ANIMATOR_HASH) || !RoundManager.Instance.powerOffPermanently) return;
 
         animator.SetBool(_POWERED_ANIMATOR_HASH, false);
         shutDownSound.Play();
+    }
+
+    [ClientRpc]
+    public void UseRandomOffsetClientRpc(float randomOffset) {
+        _randomOffset = randomOffset;
+        animator.Play(0, 0, _randomOffset);
     }
 }
